78ed2a030cd08f8709fad88b7b915126f092b021
Aria Stewart <aredridel@dinhe.net>
2020-12-16 23:01:39 -0500

    Improve the demo

--- demo.php
+++ demo.php
@@ -1,3 +1,7 @@
+<!doctype html>
+<html>
+
+<head>
 <?php
 
 require_once(__DIR__ . "/vendor/autoload.php");
@@ -5,7 +9,24 @@ require_once(__DIR__ . "/vendor/autoload.php");
 use aredridel\Blurhash\ToCss as BlurhashToCss;
 
 $hash = 'Nr8%YLkDR4j[aej]NSaznzjuk9ayR3jYofayj[f6';
-$x = new BlurhashToCss($hash);
+$o1 = new BlurhashToCss($hash);
 
-echo "Hash: $hash <br> Image: <div style='width: 300px; height: 150px; overflow: hidden; " . $x->toCss() . "'></div>";
+?>
+</head>
 
+<body>
+	<dl>
+		<dt>
+			Hash </dt>
+		<dd> <?= $hash ?> </dd>
+		<dt>
+			Image </dt>
+		<dd>
+			<div style="overflow: hidden; width: 300px; height: 150px; display: inline-block;">
+				<div style='height: 100%; width: 100%; <?= $o1->toCss() ?>'></div>
+			</div>
+		</dd>
+	</dl>
+</body>
+
+</html>
